8c131850b4733a05c1219ee00ea909c318b47662
Kapee87 <kapserker@gmail.com>
2023-06-03 12:35:58 -0300

    agregado bonton de seccion al navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,16 +5,17 @@ export default function Header() {
             <nav>
                 <div className="logo-container">
                     <img
-                        // src="https://trello.com/1/cards/646e0abe7b9593a6e3ebe12a/attachments/6471761ea2ac2bb76d9d5f90/previews/6471761fa2ac2bb76d9d5fea/download/logo_ADN_dark.png"
-                        src='src\assets\logoAdn.png'
+                        src="src\assets\logoAdn.png"
+                        // src='src\assets\logoAdn.png'
                         alt="Logo del website"
                         className="logo"
                     />
                     <h2 className="header-title">Soluciones Web</h2>
                 </div>
                 <ul>
-                    <li ><a href="#" className="team"> Equipo </a></li>
-                    <li><a href="#" className="about">Que Hacemos</a> </li>
+                    <li ><a href="#" className="team"> Inicio </a></li>
+                    <li ><a href="#" className="team"> Nosotros </a></li>
+                    <li><a href="#" className="about">Proyectos</a> </li>
                     <li><a href="#" className="contact">Contacto</a> </li>
                 </ul>
             </nav>
