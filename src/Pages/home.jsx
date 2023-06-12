e8ed28c67b9632bb3720b84bf35b798e1ce2c583
Kapee87 <kapserker@gmail.com>
2023-06-12 12:39:48 -0300

    agregado aside con redes sociales

--- src/Pages/home.jsx
+++ src/Pages/home.jsx
@@ -1,6 +1,31 @@
+import wapIcon from '../assets/whatsapp.svg'
+
+import githubIcon from '../assets/github-alt.svg'
+
+import instaIcon from '../assets/instagram.svg'
+
+
+const mockRedes = [
+    {
+        url: "wap",
+        img: wapIcon,
+    },
+    {
+        url: "github",
+        img: githubIcon,
+    },
+    {
+        url: "insta",
+        img: instaIcon,
+    },
+]
+
 
 export default function Home() {
 
+    const handleHover = (e) => {
+        console.log('mouse uo');
+    }
     return (
         <>
             <section className="home">
@@ -10,14 +35,20 @@ export default function Home() {
                         Web Solutions
                     </h1>
                 </div>
-                <div className="skew-container">
+                <div className="skew-container" >
                     <img src="https://images.unsplash.com/photo-1487058792275-0ad4aaf24ca7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fHdlYiUyMGRldmVsb3BtZW50JTIwZm9uZG8lMjBkZSUyMHBhbnRhbGxhfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60" />
+
                 </div>
+                <aside className="redesFloater" >
+                    {
+                        mockRedes.map(({ img, url}) => (
+                            <img src={img} alt="wasap icon"
+                                key={url} />
+                        ))
+                    }
+                </aside>
             </section>
 
-            {/* <aside>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, cupiditate suscipit ratione nulla nostrum iste pariatur omnis rem. Minima labore sed eaque laboriosam, rem, aspernatur quod fugit amet consequatur veritatis est alias eum doloremque. Vero aperiam amet sed reprehenderit, ullam dolores labore repellendus dolore laboriosam cum quas possimus quod dolorem! Possimus doloribus sit magnam vel sint sapiente nihil illo ipsam eligendi nam assumenda, commodi nulla harum quis minus perferendis aut quam inventore fugit laudantium culpa amet rerum? Assumenda a accusamus possimus iusto labore, necessitatibus architecto dolores quas nam vero ullam culpa numquam modi, tempore odit laudantium ipsam! Similique accusantium doloribus consequuntur cupiditate nobis vel reiciendis officiis adipisci voluptatem, odit quae laboriosam facilis enim saepe distinctio nemo velit error explicabo repellendus in neque non laborum? Corrupti earum alias minima in rem soluta repellendus quia sed aut minus est, error necessitatibus doloribus? Voluptas ex tempore consequatur ducimus quia molestias accusantium rerum consectetur dolor iste! Animi culpa, perspiciatis dignissimos libero nihil consequatur veritatis ad tempore sit excepturi, mollitia id earum suscipit ab sunt aliquam, porro esse quam ratione odit dolor officiis minima dicta! Repellat dolor placeat incidunt! Expedita maxime vel culpa possimus, provident incidunt magni? Vel, dolorem tempore? Dolorum assumenda velit nostrum quae?</p>
-            </aside> */}
         </>
     )
 }
