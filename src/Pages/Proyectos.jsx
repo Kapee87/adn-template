1e7bfb766ba03992b88bee0cbf7c29fa6cccd0e9
Kapee87 <kapserker@gmail.com>
2023-06-12 10:34:10 -0300

    retoques y responsive

--- src/Pages/Proyectos.jsx
+++ src/Pages/Proyectos.jsx
@@ -1,23 +1,37 @@
 import { thumbnail } from '../assets/thumbnail.json'
+import debbiWeb from '../assets/debbiWeb.gif'
+import arielWeb from '../assets/arielWeb.gif'
+import marcosWeb from '../assets/marcosWeb.gif'
+import lucaWeb from '../assets/lucaWeb.gif'
+import francoWeb from '../assets/francoWeb.gif'
+import kapeeWeb from '../assets/kapeeWeb.gif'
+
+
 
 const proyectsMock = [
     {
-        url: 'https://debiabril.github.io/portfolio/'
+        url: 'https://debiabril.github.io/portfolio/',
+        img: debbiWeb
     },
     {
-        url: 'https://mockup-portfolio-hardcoded-ooi0e023a-ariellevita.vercel.app'
+        url: 'https://mockup-portfolio-hardcoded-ooi0e023a-ariellevita.vercel.app',
+        img: arielWeb
     },
     {
-        url: 'https://marcoscakin.github.io/amazingEvents_Maidana/'
+        url: 'https://marcoscakin.github.io/amazingEvents_Maidana/',
+        img: marcosWeb
     },
     {
-        url: 'https://tsuramii.github.io/ADN-Solutions-Mockup/'
+        url: 'https://tsuramii.github.io/ADN-Solutions-Mockup/',
+        img: lucaWeb
     },
     {
-        url: 'http://www.franco-miguez.com'
+        url: 'http://www.franco-miguez.com',
+        img: francoWeb
     },
     {
-        url: 'https://kapee87.github.io/Barberia-Alura/'
+        url: 'https://kapee87.github.io/Barberia-Alura/',
+        img: kapeeWeb
     }
 
 ]
@@ -25,17 +39,17 @@ const proyectsMock = [
 export default function Proyectos() {
 
     return (
-        <section className="proyectos">
+        <section className="projects">
             <h2>Proyectos</h2>
             <p>Todavía no tenemos proyectos terminados para mostrar, pero de tenerlos, la grilla se vería más o menos así:
             </p>
             <div className="projectContainer">
                 {
-                    proyectsMock.map(({ url }) => (
-                        <div className="projecto" key={url}>
+                    proyectsMock.map(({ url, img }) => (
+                        <div className="project" key={url}>
                             <a href={url} target="_blank" rel="noopener noreferrer">
-                                <img src={thumbnail} alt="" />
-                                {url}
+                                <img src={img || thumbnail} alt="" />
+                                <p>{url}</p>
                             </a>
                         </div>
 
